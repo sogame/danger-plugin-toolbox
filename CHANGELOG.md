643ebca330a8173ed993388da54fcf8e873869f1
Jaume Salgado <jaume.salgado@skyscanner.net>
2018-10-27 22:47:06 +0200

    Add "commonCommitMessage" validation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+### Added
+
+- New validation [`commonCommitMessage`](docs/validations.md#commoncommitmessage) to make sure all commit messages match a regex (like ensuring a ticked id is added to all commit messages).
+
 ## [1.0.0] - 2018-10-23
 
 ### Added
---
+++ src/rules/common/commitMessage.js
@@ -0,0 +1,25 @@
+//
+// Make sure all commit messages match a regex.
+//
+
+import getMessageLogger from '../getMessageLogger';
+import { commits } from '../helpers';
+
+export const COMMON_COMMIT_MESSAGE_JIRA_REGEX = /^\[[a-zA-Z]+-\d+\]|\[NO-JIRA\]/;
+export const COMMON_COMMIT_MESSAGE_JIRA_MSG =
+  'Please include a JIRA reference like `[XXX-YYYY]` in each commit or use `[NO-JIRA]`.';
+
+export default (regex, message, { logType } = {}) => {
+  if (!regex) {
+    warn('`commonCommitMessage`: missing "regex" parameter');
+  } else if (!message) {
+    warn('`commonCommitMessage`: missing "message" parameter');
+  } else if (
+    commits.findIndex(
+      ({ message: commitMessage }) => commitMessage.match(regex) === null,
+    ) >= 0
+  ) {
+    const log = getMessageLogger(logType);
+    log(message);
+  }
+};

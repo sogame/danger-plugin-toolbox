f3440909b18c544e7b17f7c49dbcbb4e6ce3592f
Jaume Salgado <jaume.salgado@skyscanner.net>
2019-12-14 15:31:25 +0100

    Add "reactBackpackSubcomponents" validation

---
+++ src/rules/react/backpackSubcomponents.js
@@ -0,0 +1,22 @@
+//
+// Recommend using Backpack's sub-components (like BpkButtonPrimary instead of BpkButton) in React components.
+//
+
+import getMessageLogger from '../getMessageLogger';
+import { fileAddedLineMatch, committedFilesGrep } from '../helpers';
+
+const fileUsesBpButton = filename =>
+  fileAddedLineMatch(filename, /<BpkButton[\s>]/i);
+
+export default async ({ logType } = {}) => {
+  const log = getMessageLogger(logType);
+  const jsxFiles = committedFilesGrep(/\.jsx$/i);
+  jsxFiles.forEach(async filename => {
+    const usesBpkButton = await fileUsesBpButton(filename);
+    if (usesBpkButton) {
+      log(
+        `The file \`${filename}\` seems to be using \`BpkButton\`, but it is recommended to use the sub-components (like \`BpkButtonPrimary\`, \`BpkButtonSecondary\`).`,
+      );
+    }
+  });
+};
